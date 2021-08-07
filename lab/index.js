0fbb2f6f23d7e66524bc55312b0f96bf17189567
Viktor Charypar <charypar@gmail.com>
2021-08-07 17:33:36 +0100

    Pass structured data through the WASM interface, add D3 and D3FC

--- lab/index.js
+++ lab/index.js
@@ -1,11 +1,50 @@
-import init, { greet } from "./pkg/lab.js";
+import init, { reflect, to_string } from "./pkg/lab.js";
 
 const lab = async () => {
   await init();
 
-  const result = greet("Viktor");
+  const price = [
+    {
+      Open: 15320.1,
+      Low: 14970.5,
+      High: 16220.1,
+      Close: 15200.98,
+      Volume: 15000,
+    },
+    {
+      Open: 15320.1,
+      Low: 14970.5,
+      High: 16220.1,
+      Close: 15200.98,
+      Volume: 15000,
+    },
+  ];
 
-  document.getElementById("output").textContent = `Betty says: ${result}`;
+  const result = reflect(price);
+  const text = to_string(price);
+
+  console.log("from betty", result);
+
+  // D3 stuff
+
+  const data = fc.randomFinancial()(50);
+
+  const yExtent = fc.extentLinear().accessors([(d) => d.high, (d) => d.low]);
+
+  const xExtent = fc.extentDate().accessors([(d) => d.date]);
+
+  const gridlines = fc.annotationSvgGridline();
+  const candlestick = fc.seriesSvgCandlestick();
+  const multi = fc.seriesSvgMulti().series([gridlines, candlestick]);
+
+  const chart = fc
+    .chartCartesian(d3.scaleTime(), d3.scaleLinear())
+    .svgPlotArea(multi);
+
+  chart.xDomain(xExtent(data));
+  chart.yDomain(yExtent(data));
+
+  d3.select("#chart").datum(data).call(chart);
 };
 
 lab();
--- lab/src/lib.rs
+++ lab/src/lib.rs
@@ -1,6 +1,34 @@
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct PriceRecord {
+    #[serde(rename = "Open")]
+    open: f32,
+    #[serde(rename = "High")]
+    high: f32,
+    #[serde(rename = "Low")]
+    low: f32,
+    #[serde(rename = "Close")]
+    close: f32,
+    #[serde(rename = "Volume")]
+    volume: f32,
+}
+
 #[wasm_bindgen]
-pub fn greet(subject: String) -> String {
-    format!("Hello, {}!", subject)
+pub fn reflect(record: JsValue) -> JsValue {
+    if let Ok(value) = record.into_serde::<Vec<PriceRecord>>() {
+        JsValue::from_serde(&value).unwrap()
+    } else {
+        let value: Vec<PriceRecord> = vec![];
+        JsValue::from_serde(&value).unwrap()
+    }
+}
+
+#[wasm_bindgen]
+pub fn to_string(record: JsValue) -> String {
+    match record.into_serde::<Vec<PriceRecord>>() {
+        Ok(record) => format!("{:#?}!", record),
+        Err(err) => format!("{}", err),
+    }
 }
